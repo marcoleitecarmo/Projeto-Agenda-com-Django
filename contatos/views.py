42500aac564c314a2ad8426e3af40a9920ccfaaf
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2021-12-28 21:14:36 -0300

    Agenda

---
+++ contatos/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from . models import Contato
+
+# Create your views here.
+
+
+def index(request):
+    contatos = Contato.objects.all()
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+
+def ver_contato(request, contato_id):
+    # contato = Contato.objects.get()
+    contato = get_object_or_404(Contato, id=contato_id )
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
