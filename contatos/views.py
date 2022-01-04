026fdea6379cc2c8f4d05536d6bd063f8ab89bee
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2022-01-04 10:22:03 -0300

    melhorias na agenda

--- contatos/views.py
+++ contatos/views.py
@@ -1,11 +1,18 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from . models import Contato
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 20)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
@@ -14,6 +21,10 @@ def index(request):
 def ver_contato(request, contato_id):
     # contato = Contato.objects.get()
     contato = get_object_or_404(Contato, id=contato_id )
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
